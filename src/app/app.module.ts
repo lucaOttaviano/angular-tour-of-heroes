10ca572d708b44edf4b1d71a9ee43c9313a7352b
lucaOttaviano <luca.ottaviano.lavoro@gmail.com>
2022-04-11 11:21:27 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,27 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './components/routing/app-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,24 +3,39 @@ import { delay, Observable, of } from 'rxjs';
 import { HEROESMOCKDATA } from '../mock-data/mock-heroes';
 import { Hero } from '../models/hero';
 import { MessageService } from './message.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { } // iniezione del servizio tramite costruttore
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+  constructor(private messageService: MessageService, private http:HttpClient) { } // iniezione del servizio tramite costruttore, inetto l'httpclient
 
   getHeroes(): Observable<Hero[]>{ 
-     const HEROES: Observable<Hero[]> = of(HEROESMOCKDATA).pipe(delay(1000)); // l'of simula una chiamata ad un server
-     this.messageService.add('HeroService: fetched heroes');
-     return HEROES;
+    return this.http.get<Hero[]>(this.heroesUrl) // richiama l'url
+      .pipe(
+        tap(_ => this.log('fetched heroes')), // richiama la funzione log() 
+        catchError(error => { // all'errrore
+          console.error(error); // stampa l'errore
+          this.log(`getHeroes failed: ${error.status}: ${error.message}`); // logga 
+          let response = [] as Hero[];
+          return of(response);
+        })
+      )
   }
 
   getHero(selectedId: number): Observable<Hero> {
-    const hero = HEROESMOCKDATA.find(h => h.id === selectedId)!;
-    this.messageService.add(`HeroService fetched hero id=${selectedId}`);
-    return of (hero);
+    return this.http.get<Hero>(this.heroesUrl + '/' + selectedId) // inserisce l'id selezionato nell'url
+    .pipe(
+      tap(_ => this.log(`fetched hero id = ${selectedId}`))
+    );
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
 }
